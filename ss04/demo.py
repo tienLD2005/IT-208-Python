a1cd97aa7c058f815ef9e4bef89b57571b04230e
Tiến LD <ledientien2005@gmail.com>
2025-11-21 00:28:10 +0700

    done ss04

---
+++ ss04/demo.py
@@ -0,0 +1,14 @@
+myList = [1,"hello", True, None, 1.2]
+print(f"my list truoc khi thao tac = {myList}")
+#1.1 thêm phần tử vào cuối list -> append
+myList.append("Le dien Tien")
+print(f"my list sau khi append = {myList}")
+#1.2 thêm phần tử vào 1 vị trí chỉ định
+myList.insert(1,"Gud bye")
+print(f"my list sau khi insertt = {myList}")
+
+userInfo = {
+    "name": "Le Dien Tien",
+    "age" : 20
+}
+print(userInfo)
